b1456c3429d3f4fcd0738055c325991d97915d4e
Rndynt <rendyanta.maulana@gmail.com>
2026-06-21 01:25:00 +0700

    fix(pos): hide internal service capability panel

--- apps/pos-terminal-web/src/features/pos-flows/service/ServiceCorePOSFlow.tsx
+++ apps/pos-terminal-web/src/features/pos-flows/service/ServiceCorePOSFlow.tsx
@@ -1,13 +1,7 @@
 import { RetailStandardPOSFlowView } from "@/features/pos-flows/retail";
-import { ServiceOptionalPanels } from "./ServiceOptionalPanels";
 import { useServiceCorePOSFlow } from "./useServiceCorePOSFlow";
 
 export function ServiceCorePOSFlow() {
   const flow = useServiceCorePOSFlow();
-  return (
-    <div data-testid="service-core-pos-flow">
-      <ServiceOptionalPanels capabilities={flow.capabilities} />
-      <RetailStandardPOSFlowView flow={flow} />
-    </div>
-  );
+  return <RetailStandardPOSFlowView flow={flow} />;
 }
